df511fe9caab3c2460dfc0e41d70986285876fe1
lucianoamarante <166272391+lucianoamarante@users.noreply.github.com>
2025-05-03 00:45:00 -0300

    Commit Ótica LENS Beta 00.2D Colocando os nav_pesquisar e no menu sanduiche o icone pesquisar.

--- LENS/app/src/main/java/br/com/oticaslens/MainActivity.kt
+++ LENS/app/src/main/java/br/com/oticaslens/MainActivity.kt
@@ -38,7 +38,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home,
+                R.id.nav_gallery,
+                R.id.nav_slideshow,
+                R.id.nav_pesquisar
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
